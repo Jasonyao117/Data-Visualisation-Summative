74c964785037581699d881cedc47b83366bb7b1d
Rui Zhou <845865399@cit3-501m-10.acg.ac.nz>
2018-09-17 13:41:26 +1200

    team linked

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,5 @@
 $(function(){
+	console.log("hi");
 	let key = 'SoHrJ7hCREJMhtXKdvUOGhmKGNdXp6be';
 
 	let projectHTML = $('#templateProject').text();
@@ -14,7 +15,7 @@ $(function(){
 		success:function(res){
 			// console.log(res);
 			_(res.projects).each(function(project){
-				// console.log(project);
+				console.log(project);
 
 				let output = projectTemplate(project);
 				$('.project-container').append(output);
@@ -39,7 +40,7 @@ $(function(){
 			success:function(res){
 				let project = res.project;
 
-				console.log(project);
+				// console.log(project);
 
 				var output = singleProjectTemplate(project);
 				$('.single-project-container').empty();
@@ -49,6 +50,41 @@ $(function(){
 			}
 	    });
     });
+
+
+    //user details
+    
+
+    let teamMemberHTML = $('#templateTeamMember').text();
+	let teamMemberTemplate = Template7(teamMemberHTML).compile();
+
+	function getUserDetails(username){
+
+		let urlUser = 'https://api.behance.net/v2/users/'+username+'?client_id=' + key;
+
+		$.ajax({
+			url:urlUser,
+			dataType:'jsonp',
+			success:function(res){
+		
+
+				var user = res.user;
+				// _(res.projects).each(function(project){
+				console.log(user);
+
+				let output = teamMemberTemplate(user);
+				$('.team-members').append(output);
+					
+				// });
+			}
+	    });
+	}
+	
+	getUserDetails('melkhiah');
+	getUserDetails('blugraphic');
+	getUserDetails('MartaBevacqua');
+	getUserDetails('kline_DS');
+
 
 
 	
