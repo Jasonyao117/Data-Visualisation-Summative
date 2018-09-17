c8541f2a7ac05a99dcb74d83fafb6b2883595156
Rui Zhou <845865399@cit3-501m-10.acg.ac.nz>
2018-09-17 12:46:30 +1200

    works import

---
+++ js/myscript.js
@@ -0,0 +1,57 @@
+$(function(){
+	let key = 'SoHrJ7hCREJMhtXKdvUOGhmKGNdXp6be';
+
+	let projectHTML = $('#templateProject').text();
+	let projectTemplate = Template7(projectHTML).compile();
+
+
+	let urlProjects = 'https://api.behance.net/v2/users/blugraphic/projects?client_id=' + key;
+
+
+	$.ajax({
+		url:urlProjects,
+		dataType:'jsonp',
+		success:function(res){
+			// console.log(res);
+			_(res.projects).each(function(project){
+				// console.log(project);
+
+				let output = projectTemplate(project);
+				$('.project-container').append(output);
+				
+			});
+		}
+    });
+
+
+    //Show individual projects
+    let singleProjectHTML = $('#templateSingleProject').text();
+    let singleProjectTemplate = Template7(singleProjectHTML).compile();
+
+    $('#portfolioModal1').on('show.bs.modal',function(e){
+    	let target = e.relatedTarget;
+    	let projectid = $(target).data('projectid');
+    	let urlProject = 'http://www.behance.net/v2/projects/'+projectid+'?api_key=' + key;
+
+    	$.ajax({
+			url:urlProject,
+			dataType:'jsonp',
+			success:function(res){
+				let project = res.project;
+
+				console.log(project);
+
+				var output = singleProjectTemplate(project);
+				$('.single-project-container').empty();
+				$('.single-project-container').append(output);
+
+				
+			}
+	    });
+    });
+
+
+	
+
+
+});
