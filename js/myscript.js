c0d6583e3e33995eeacaf4cc786387e447c3bce5
Judy <judy@MayaShens-MacBook-Pro.local>
2018-09-24 11:51:40 +1200

    portfolio id resolved

--- js/myscript.js
+++ js/myscript.js
@@ -2,8 +2,8 @@ $(function(){
 	// let key = '5LD1TzsJTngQ7kWNRCsMtTzgCTYUeKKM';
 	// let key = 'M3frM30OjgSt9DkMioiPPPeXwnQRdDIq';
 	// let key = 'REcMwpYUbD95giMsJ20juYVPQBV2H3QI';
-	let key = 'OYfT28SqRnMg4IICU3k0v0XHocyLvIHm';
-	// let key = 'j09nGbsYVA4iwksNYE9JCwF0kll1dKuQ';
+	// let key = 'OYfT28SqRnMg4IICU3k0v0XHocyLvIHm';
+	let key = 'j09nGbsYVA4iwksNYE9JCwF0kll1dKuQ';
 
 
 	
